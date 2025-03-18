d64447d9f3f82200e9c0e1435162d1d7d8f1382c
Antonio Nogues <anogues23@gmail.com>
2025-03-18 19:28:21 +0100

    🎉Primer Commit

---
+++ src/main/java/com/example/skystayback/SkyStayBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.skystayback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyStayBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyStayBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/skystayback/SkyStayBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skystayback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyStayBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
