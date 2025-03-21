2a2b4618a6284fc8a1df6870f2ea9d1ccbf89a95
Rodrigorjc <rjaencobos@safareyes.es>
2025-03-21 19:59:56 +0100

    ✨ Creación del GlobalExceptionHandler.

---
+++ src/main/java/com/example/skystayback/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.example.skystayback.exceptions;
+
+import com.example.skystayback.dtos.ErrorResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ApiException.class)
+    public ResponseEntity<ErrorResponseDTO> handleApiException(ApiException ex) {
+        ErrorResponseDTO errorResponse = ErrorResponseDTO.builder()
+                .title(ex.getTitle())
+                .message(ex.getMessage())
+                .errorCode(ex.getErrorCode())
+                .timestamp(LocalDateTime.now())
+                .build();
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<ErrorResponseDTO> handleUsernameNotFoundException(UsernameNotFoundException ex) {
+        ErrorResponseDTO errorResponse = ErrorResponseDTO.builder()
+                .title("Usuario no encontrado")
+                .message(ex.getMessage())
+                .errorCode("USER_NOT_FOUND")
+                .timestamp(LocalDateTime.now())
+                .build();
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+}
