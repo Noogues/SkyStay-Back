aefb59d61346f03b102db196390244c1098669ae
Rodrigorjc <rjaencobos@safareyes.es>
2025-03-22 20:59:56 +0100

    🐛 Se ha cambiado el id de bigint a long en AirlineRating.

--- src/main/java/com/example/skystayback/models/AirlineRating.java
+++ src/main/java/com/example/skystayback/models/AirlineRating.java
@@ -13,7 +13,7 @@ public class AirlineRating {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false, unique = true)
-    private BigInteger id;
+    private Long id;
 
     @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id")
