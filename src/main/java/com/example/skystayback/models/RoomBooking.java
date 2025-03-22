9b0f4cba124830068f897501b21bd0bdd33e9e7c
Rodrigorjc <rjaencobos@safareyes.es>
2025-03-22 21:04:07 +0100

    🐛 Se ha cambiado el id de bigint a long y se han agregado los campos q faltaban a RoomBooking.

--- src/main/java/com/example/skystayback/models/RoomBooking.java
+++ src/main/java/com/example/skystayback/models/RoomBooking.java
@@ -12,9 +12,17 @@ public class RoomBooking {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false, unique = true)
-    private BigInteger id;
+    private Long id;
 
     @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
     @JoinColumn(name = "hotel_id")
     private Hotel hotel;
+
+    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
 }
