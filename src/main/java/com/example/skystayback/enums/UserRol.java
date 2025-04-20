c56d7315fa43bf90dd9ce19d259353683809631a
Antonio Nogues <anogues23@gmail.com>
2025-04-20 21:18:26 +0200

    🔧 Refactor UserRol: implementar GrantedAuthority y renombrar roles a ROLE_ADMIN, ROLE_MODERATOR, ROLE_CLIENT

--- src/main/java/com/example/skystayback/enums/UserRol.java
+++ src/main/java/com/example/skystayback/enums/UserRol.java
@@ -1,5 +1,13 @@
 package com.example.skystayback.enums;
 
-public enum UserRol {
-    ADMIN, MODERATOR, CLIENT
+import org.springframework.security.core.GrantedAuthority;
+
+public enum UserRol implements GrantedAuthority {
+    ROLE_ADMIN, ROLE_MODERATOR, ROLE_CLIENT;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
 }
+
