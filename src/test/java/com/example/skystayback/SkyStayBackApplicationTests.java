84280552c1d75376fc03a082c156dfd42dd982b6
Antonio Nogues <anogues23@gmail.com>
2025-04-20 21:19:03 +0200

    🔧 Refactor pruebas: agregar configuración de pruebas para AdminService y UserRepository

--- src/test/java/com/example/skystayback/SkyStayBackApplicationTests.java
+++ src/test/java/com/example/skystayback/SkyStayBackApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.skystayback;
 
-import org.junit.jupiter.api.Test;
+import com.example.skystayback.repositories.UserRepository;
+import com.example.skystayback.services.AdminService;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SkyStayBackApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Mock
+    private UserRepository userRepository;
 
+    @InjectMocks
+    private AdminService adminService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
 }
