6738223a0c28d1303263a24b4d4d6f388fccb73c
Antonio Nogues <anogues23@gmail.com>
2025-05-03 11:34:20 +0200

    🔧 Agregar métodos para obtener información básica de aviones y asientos por código de avión y por ID de cabina en los repositorios correspondientes

--- src/main/java/com/example/skystayback/repositories/AirplaneRepository.java
+++ src/main/java/com/example/skystayback/repositories/AirplaneRepository.java
@@ -1,12 +1,18 @@
 package com.example.skystayback.repositories;
 
+import com.example.skystayback.dtos.airplanes.AirplaneAllCodeVO;
 import com.example.skystayback.dtos.airplanes.AirplaneShowVO;
+import com.example.skystayback.dtos.airplanes.CabinVO;
+import com.example.skystayback.dtos.airplanes.SeatVO;
 import com.example.skystayback.models.Airplane;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 
 @Repository
 public interface AirplaneRepository extends JpaRepository<Airplane, Long> {
@@ -27,6 +33,25 @@ public interface AirplaneRepository extends JpaRepository<Airplane, Long> {
             )
         )
         FROM Airplane a JOIN a.airplaneType at
+        ORDER BY a.id ASC
     """)
     Page<AirplaneShowVO> getAllAirplanes(Pageable pageable);
+
+    @Query(value = """
+    SELECT new com.example.skystayback.dtos.airplanes.AirplaneAllCodeVO(
+        a.model,
+        a.registrationNumber,
+        a.yearOfManufacture,
+        a.type,
+        a.status,
+        at.name,
+        at.manufacturer,
+        at.capacity
+    )
+    FROM Airplane a
+    JOIN a.airplaneType at
+    WHERE a.code = :airplaneCode
+""")
+    AirplaneAllCodeVO findBasicInfoByCode(String airplaneCode);
+
 }
