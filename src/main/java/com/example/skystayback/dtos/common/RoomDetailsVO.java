7f3d81aafcc058325542d707f7330bfcadde442d
Rodrigorjc <rjaencobos@safareyes.es>
2025-06-07 22:45:22 +0200

    ✨ Añadir campo price en la clase RoomDetailsVO para almacenar información sobre el precio de la habitación

--- src/main/java/com/example/skystayback/dtos/common/RoomDetailsVO.java
+++ src/main/java/com/example/skystayback/dtos/common/RoomDetailsVO.java
@@ -15,4 +15,13 @@ public class RoomDetailsVO {
     private Integer capacity;
     private RoomType roomType;
     private Long availableCount;
+    private Double price;
+
+    public RoomDetailsVO(Long id, Integer capacity, String type, Integer availableRooms, Double price) {
+        this.roomConfigId = id;
+        this.capacity = capacity;
+        this.roomType = RoomType.valueOf(type);
+        this.availableCount = Long.valueOf(availableRooms);
+        this.price = price;
+    }
 }
