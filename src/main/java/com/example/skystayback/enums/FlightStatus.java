5235a68421945c333ab1d606ed3b7832233c40a0
Antonio Nogues <anogues23@gmail.com>
2025-06-08 11:45:41 +0200

    🔨 Actualizar comentarios en el enum FlightStatus para incluir valores numéricos asociados a cada estado de vuelo

--- src/main/java/com/example/skystayback/enums/FlightStatus.java
+++ src/main/java/com/example/skystayback/enums/FlightStatus.java
@@ -1,11 +1,11 @@
 package com.example.skystayback.enums;
 
 public enum FlightStatus {
-    SCHEDULED,   // Programado
-    BOARDING,    // Embarcando
-    IN_FLIGHT,   // En vuelo
-    LANDED,      // Aterrizado
-    CANCELLED,   // Cancelado
-    DELAYED,     // Retrasado
-    DIVERTED     // Desviado a otro aeropuerto
+    SCHEDULED,   // Programado | 0
+    BOARDING,    // Embarcando | 1
+    IN_FLIGHT,   // En vuelo | 2
+    LANDED,      // Aterrizado | 3
+    CANCELLED,   // Cancelado | 4
+    DELAYED,     // Retrasado | 5
+    DIVERTED     // Desviado a otro aeropuerto |6
 }
