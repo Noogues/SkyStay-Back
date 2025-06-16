18887c665531b477de2df40eec88960bcc319b87
Antonio Nogues <anogues23@gmail.com>
2025-06-16 19:54:57 +0200

    🔧Cambiar configuración de arranque para las variables

--- src/main/java/com/example/skystayback/SkyStayBackApplication.java
+++ src/main/java/com/example/skystayback/SkyStayBackApplication.java
@@ -2,6 +2,7 @@ package com.example.skystayback;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
@@ -9,18 +10,25 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class SkyStayBackApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-        System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
-        System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
-        System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
-        System.setProperty("DB_USER", dotenv.get("DB_USER"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-        System.setProperty("EM_HOST", dotenv.get("EM_HOST"));
-        System.setProperty("EM_PORT", dotenv.get("EM_PORT"));
-        System.setProperty("EM_USERNAME", dotenv.get("EM_USERNAME"));
-        System.setProperty("EM_PASSWORD", dotenv.get("EM_PASSWORD"));
+        org.springframework.boot.SpringApplication app = new org.springframework.boot.SpringApplication(SkyStayBackApplication.class);
+        Environment env = app.run(args).getEnvironment();
 
-        org.springframework.boot.SpringApplication.run(SkyStayBackApplication.class, args);
+        String activeProfile = env.getProperty("spring.profiles.active", "default");
+
+        if ("dev".equals(activeProfile)) {
+            if (new java.io.File(".env").exists()) {
+                Dotenv dotenv = Dotenv.load();
+                System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+                System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+                System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+                System.setProperty("DB_USER", dotenv.get("DB_USER"));
+                System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+                System.setProperty("EM_HOST", dotenv.get("EM_HOST"));
+                System.setProperty("EM_PORT", dotenv.get("EM_PORT"));
+                System.setProperty("EM_USERNAME", dotenv.get("EM_USERNAME"));
+                System.setProperty("EM_PASSWORD", dotenv.get("EM_PASSWORD"));
+            }
+        }
     }
 
 }
